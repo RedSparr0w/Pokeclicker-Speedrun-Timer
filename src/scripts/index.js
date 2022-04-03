576c370f88d422b9ba57f658f90b4f568e4b6ff2
Danial <admin@redsparr0w.com>
2022-04-03 20:31:41 +1200

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,4 @@ s.src = chrome.runtime.getURL('scripts/inject.js');
 s.onload = function() {
     this.remove();
 };
-console.log('src', s.src);
 (document.head || document.documentElement).appendChild(s);
