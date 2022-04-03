a18de8948fa5b9c2d8455aed36a6a3f9790bc6e0
Danial <admin@redsparr0w.com>
2022-04-04 10:18:51 +1200

    Track based on speedrun type

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -1,8 +1,14 @@
-(() => {
-  const logInfo = (...args) => {
-    console.info('%c[PCSRT]', 'color: red;', ...args)
-  }
+const SpeedrunType = {
+  tutorial: 0,
+  kanto_champion: 1,
+  kanto_pokedex: 2
+}
 
+const SpeedrunOptions = {
+  type: 0,
+};
+
+(() => {
   const injectMethodBefore = (object, method, callback) => {
     const original = object[method].bind(object);
     
@@ -10,7 +16,7 @@
       callback(...arguments);
       original(...arguments);
     };
-  }
+  };
 
   const injectMethodAfter = (object, method, callback) => {
     const original = object[method].bind(object);
@@ -19,38 +25,86 @@
       original(...arguments);
       callback(...arguments);
     };
-  }
+  };
+
+  const logInfo = (...args) => {
+    console.info('%c[PCSRT]', 'color: red;', ...args)
+  };
 
   logInfo('PokeClicker Speedrun Timer Enabled!');
 
   let startTime = null;
+  let endTime = null;
   injectMethodAfter(StartSequenceRunner, 'start', () => {
     logInfo('Game Loaded, Injecting hooks.');
+
+    // Add our timer container
     const timerContainer = document.createElement('div');
     timerContainer.id = 'speedrunTimerContainer';
     timerContainer.classList.add('card', 'sortable', 'border-secondary', 'mb-3');
     timerContainer.innerHTML = `
     <div class="card-header p-0" data-toggle="collapse" href="#speedrunTimerBody">
-        <span style="text-align:center">Speedrun Timer</span>
+      <span style="text-align:center">Speedrun Timer</span>
     </div>
-    <div id="speedrunTimerBody" class="card-body show p-0">
-      <table id="speedrunTimerTable" class="table table-sm m-0">
-        <tbody>
-          <tr>
-            <th class="p-0" colspan="3">
-              <h3 id="speedrunTimerClock" class="text-primary">00:00:00</h3>
-              <small id="speedrunTimerClockMS" class="text-primary">.000</small>
-            </th>
-          </tr>
-        </tbody>
-      </table>
+    <div class="card-body p-0">
+      <h3 id="speedrunTimerClock" class="text-primary font-weight-bold">0</h3>
+      <small id="speedrunTimerClockMS" class="text-primary font-weight-bold" style="margin-left: -5px">.000</small>
+      <div id="speedrunTimerBody" class="show">
+        <table id="speedrunTimerTable" class="table table-sm m-0">
+          <tbody>
+          </tbody>
+        </table>
+      </div>
     </div>`;
     document.getElementById('right-column').appendChild(timerContainer);
 
-    injectMethodAfter(Battle, 'defeatPokemon', () => {
-      // Only run if we dont have a start time
-      if (startTime) return;
+    // Add our speedrun options
+    const speedrunModal = document.createElement('div');
+    speedrunModal.id = 'speedrunModal';
+    speedrunModal.classList.add('modal', 'noselect', 'fade');
+    speedrunModal.role = 'dialog';
+    speedrunModal.ariaLabel = 'speedrunModal';
+    speedrunModal.innerHTML = `
+    <div class="modal-dialog modal-dialog-scrollable modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Speedrun Options</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h3>Type</h3>
+          <div class="btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-secondary btn-block active">
+              <input type="radio" name="speedrunType" id="speedrunType0" value="0" onchange="SpeedrunOptions.type = this.value" checked> Tutorial
+            </label>
+            <label class="btn btn-secondary btn-block">
+              <input type="radio" name="speedrunType" id="speedrunType1" value="1" onchange="SpeedrunOptions.type = this.value"> Kanto Champion
+            </label>
+            <label class="btn btn-secondary btn-block">
+              <input type="radio" name="speedrunType" id="speedrunType2" value="2" onchange="SpeedrunOptions.type = this.value"> 151 Pokemon
+            </label>
+          </div>
+          <br/>
+          <button class="btn btn-primary btn-block" data-dismiss="modal">Done!</button>
+        </div>
+      </div>
+    </div>`;
+    document.body.appendChild(speedrunModal);
 
+    const speedrunButton = document.createElement('a');
+    speedrunButton.classList.add('btn', 'btn-primary', 'mr-0');
+    speedrunButton.href = '#speedrunModal';
+    speedrunButton.dataset.toggle = 'modal';
+    speedrunButton.innerText = 'Speedrun Options';
+    document.querySelector('#startSequenceModal .modal-footer').prepend(speedrunButton);
+
+    injectMethodAfter(Battle, 'defeatPokemon', () => {
+      // Only run when we defeat our first pokemon
+      if (App.game.statistics.totalPokemonDefeated() > 1) return
+
+      // Lets start our timer
       logInfo('Started timer!');
 
       startTime = new Date();
@@ -58,29 +112,73 @@
       const clockElement = document.getElementById('speedrunTimerClock');
       const msElement = document.getElementById('speedrunTimerClockMS');
       const updateTimer = (delta) => {
-        const now = Date.now();
+        if (!startTime) return;
+
+        // If we finished the run
+        let now = endTime || new Date();
         const time = new Date(now - startTime);
-        clockElement.innerText = time.toISOString().substring(11, 19);
+        clockElement.innerText = time.toISOString().substring(11, 19).replace(/^[0:]+/, '') || '0';
         msElement.innerText = time.toISOString().substring(19, 23);
-        requestAnimationFrame(updateTimer);
+        if (!endTime) requestAnimationFrame(updateTimer);
+        else {
+          clockElement.classList.remove('text-primary');
+          clockElement.classList.add('text-success');
+          msElement.classList.remove('text-primary');
+          msElement.classList.add('text-success');
+        }
       }
       requestAnimationFrame(updateTimer);
       
     });
 
     injectMethodBefore(App.game.party, 'gainPokemonById', (id, shiny) => {
+      // If run ended
+      if (endTime) return;
+
+      // Only add a split if the pokemon is new
       if (!App.game.party.alreadyCaughtPokemon(id)) {
-        // Splits on pokemon caught?
+        // Add one as we are running this method before we catch the pokemon
+        const count = App.game.party.caughtPokemon.length + 1;
+
+        if (count % 20 == 0 || count == 151) {
+          logInfo(`Pokemon Caught: ${count}!`);
+  
+          const now = new Date();
+          const time = new Date(now - startTime);
+
+          if (SpeedrunOptions.type == SpeedrunType.kanto_pokedex && count == 151) {
+            endTime = now;
+          }
+  
+          const table = document.getElementById('speedrunTimerTable');
+          const row = table.insertRow();
+          badgeCell = row.insertCell();
+          badgeCell.innerText = `${count} Pokemon`;
+          timeCell = row.insertCell();
+          timeCell.innerText = time.toISOString().substring(11, 23).replace(/^[0:]+/, '');
+        }
       }
     });  
 
     injectMethodBefore(App.game.badgeCase, 'gainBadge', badge => {
+      // If run ended
+      if (endTime) return;
+
+      // Only add a split if the badge is new
       if (!App.game.badgeCase.hasBadge(badge)) {
         // Splits on badge gained
         logInfo(`Badge Earned: ${GameConstants.camelCaseToString(BadgeEnums[badge])}!`);
 
-        const now = Date.now();
+        const now = new Date();
         const time = new Date(now - startTime);
+
+        if (SpeedrunOptions.type == SpeedrunType.tutorial && badge == BadgeEnums.Boulder) {
+          endTime = now;
+        }
+
+        if (SpeedrunOptions.type == SpeedrunType.kanto_champion && badge == BadgeEnums.Elite_KantoChampion) {
+          endTime = now;
+        }
 
         const table = document.getElementById('speedrunTimerTable');
         const row = table.insertRow();
